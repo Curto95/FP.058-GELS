600febd79feb368ca7cf7df298a8828f0513ff31
GuitoReus <128318597+GuitoReus@users.noreply.github.com>
2023-04-24 16:37:16 +0200

    Add files via upload

---
+++ FP.058-GELS-Producto3/src/main/java/excepciones/FechaExcepcion.java
@@ -0,0 +1,14 @@
+//Paquetes
+package excepciones;
+/**
+ *
+ * @author gels
+ */
+public class FechaExcepcion extends Exception {
+
+    private static final long serialVersionUID = 21L;
+
+    public FechaExcepcion(String msg) {
+        super(msg);
+    }
+}
